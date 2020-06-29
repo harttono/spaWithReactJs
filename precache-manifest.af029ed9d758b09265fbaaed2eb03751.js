d61bfaf9206b242d042fe88b11d21a9d731023aa
harttonz <harttonzzurkerbeg@gmail.com>
2020-06-29 17:48:07 +0700

    Updates

---
+++ precache-manifest.af029ed9d758b09265fbaaed2eb03751.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f9f5f19b1d614de0f46ae6901af7fee",
+    "url": "/spaWithReactJs/index.html"
+  },
+  {
+    "revision": "7e2be0eec654c0933705",
+    "url": "/spaWithReactJs/static/css/main.f572b62d.chunk.css"
+  },
+  {
+    "revision": "7d9f8488d1f587786fdd",
+    "url": "/spaWithReactJs/static/js/2.746e16d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spaWithReactJs/static/js/2.746e16d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e2be0eec654c0933705",
+    "url": "/spaWithReactJs/static/js/main.7d94058f.chunk.js"
+  },
+  {
+    "revision": "c581085708842196bb65",
+    "url": "/spaWithReactJs/static/js/runtime-main.febe865c.js"
+  }
+]);
